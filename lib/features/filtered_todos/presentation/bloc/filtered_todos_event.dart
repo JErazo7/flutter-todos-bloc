227221872da43e7f26e237bc2a2e1b96fc32451b
Josue <jderazo4@espe.edu.ec>
2019-12-29 10:47:46 -0500

    Implementado bloc de filtered_todos

--- lib/features/filtered_todos/presentation/bloc/filtered_todos_event.dart
+++ lib/features/filtered_todos/presentation/bloc/filtered_todos_event.dart
@@ -1,5 +1,31 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter_todos_bloc/features/filtered_todos/domain/entities/visibility_filter.dart';
+import 'package:flutter_todos_bloc/features/todos/domain/entities/todo.dart';
 
 abstract class FilteredTodosEvent extends Equatable {
   const FilteredTodosEvent();
+}
+
+class UpdateFilter extends FilteredTodosEvent {
+  final VisibilityFilter filter;
+
+  const UpdateFilter(this.filter);
+
+  @override
+  List<Object> get props => [filter];
+
+  @override
+  String toString() => 'UpdateFilter { filter: $filter }';
+}
+
+class UpdateTodos extends FilteredTodosEvent {
+  final List<Todo> todos;
+
+  const UpdateTodos(this.todos);
+
+  @override
+  List<Object> get props => [todos];
+
+  @override
+  String toString() => 'UpdateTodos { todos: $todos }';
 }
--- lib/features/filtered_todos/presentation/bloc/filtered_todos_state.dart
+++ lib/features/filtered_todos/presentation/bloc/filtered_todos_state.dart
@@ -1,10 +1,30 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter_todos_bloc/features/filtered_todos/domain/entities/visibility_filter.dart';
+import 'package:flutter_todos_bloc/features/todos/domain/entities/todo.dart';
 
 abstract class FilteredTodosState extends Equatable {
   const FilteredTodosState();
-}
 
-class InitialFilteredTodosState extends FilteredTodosState {
   @override
   List<Object> get props => [];
+}
+
+class FilteredTodosLoading extends FilteredTodosState {}
+
+class FilteredTodosLoaded extends FilteredTodosState {
+  final List<Todo> filteredTodos;
+  final VisibilityFilter activeFilter;
+
+  const FilteredTodosLoaded(
+    this.filteredTodos,
+    this.activeFilter,
+  );
+
+  @override
+  List<Object> get props => [filteredTodos, activeFilter];
+
+  @override
+  String toString() {
+    return 'FilteredTodosLoaded { filteredTodos: $filteredTodos, activeFilter: $activeFilter }';
+  }
 }
