d4e4b7a37c335e4a64d84822095155ade0710414
Josue <jderazo4@espe.edu.ec>
2019-12-29 11:09:28 -0500

    Implementado barril con todos los blocs y un blocDelegate

---
+++ lib/features/blocs.dart
@@ -0,0 +1,5 @@
+export './simple_bloc_delegate.dart';
+export './filtered_todos/presentation/bloc/bloc.dart';
+export './stats//presentation/bloc/bloc.dart';
+export './tab//presentation/bloc/bloc.dart';
+export './todos//presentation/bloc/bloc.dart';
